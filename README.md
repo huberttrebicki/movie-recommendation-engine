81cd6fac369aa29365032f24def5a4d0098ed447
Hubert Trebicki <hubertrebicki@gmail.com>
2026-03-13 21:17:36 +0100

    feat(movies): add title based recommendations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # movie-recommendation-engine
+Movie recommendation engine based on neo4j graph database with the TMDB 5000 dataset.
+The api layer was implemented using gin framework.
--- main.go
+++ main.go
@@ -69,10 +69,66 @@ func main() {
 			"movies": res,
 		})
 	})
+	router.GET("movies/:title/recommended", func(ctx *gin.Context) {
+		title := ctx.Param("title")
+		res, err := getMovieRecommendationsBasedOnTitle(c, driver, dbName, title)
+		if err != nil {
+			log.Fatalf("Error when executing query: %v", err)
+		}
+		ctx.JSON(200, gin.H{
+			"basedOn": title,
+			"movies":  res,
+		})
+	})
 	router.Run()
 }
 
-func getMovieRecommendationsBasedOnTitle(ctx context.Context, driver neo4j.Driver, title string) {
+func getMovieRecommendationsBasedOnTitle(ctx context.Context, driver neo4j.Driver, dbName, title string) ([]map[string]any, error) {
+	params := map[string]any{
+		"title": title,
+	}
+
+	// NOTE: query doesn't work the best with franchises, though dataset didn't have any connections
+	cipher := `
+	MATCH (target:Movie {title: $title})
+	WITH target,
+      [g IN apoc.convert.fromJsonList(target.genres) | g.name] AS targetGenres,
+      [k IN apoc.convert.fromJsonList(target.keywords) | k.name] AS targetKeywords
+    MATCH (rec:Movie)
+    WHERE rec.id <> target.id AND rec.vote_count > 100
+    WITH target, targetGenres, targetKeywords, rec,
+      [g IN apoc.convert.fromJsonList(rec.genres) | g.name] AS recGenres,
+      [k IN apoc.convert.fromJsonList(rec.keywords) | k.name] AS recKeywords
+    WITH target, rec, recGenres, recKeywords,
+      size([g IN recGenres WHERE g IN targetGenres]) AS sharedGenres,
+      size([k IN recKeywords WHERE k IN targetKeywords]) AS sharedKeywords
+    OPTIONAL MATCH (target)<-[:ACTED_IN|DIRECTED]-(p:Person)-[:ACTED_IN|DIRECTED]->(rec)
+    WITH rec, recGenres, recKeywords, sharedGenres, sharedKeywords, count(p) AS sharedPeople
+    WITH rec, recGenres, recKeywords, (sharedPeople * 5) + (sharedGenres * 2) + (sharedKeywords * 1) AS TotalScore
+    WHERE TotalScore > 0
+    ORDER BY TotalScore DESC, rec.vote_average DESC
+    LIMIT 15
+    RETURN
+      rec.title AS Title,
+      TotalScore,
+      rec.vote_average AS Rating,
+      recGenres AS Genres,
+      rec.overview AS Overview,
+      rec.runtime AS Runtime,
+      recKeywords AS Keywords,
+      rec.release_date AS ReleaseDate
+	`
+
+	res, err := neo4j.ExecuteQuery(ctx, driver, cipher, params, neo4j.EagerResultTransformer, neo4j.ExecuteQueryWithDatabase(dbName))
+	if err != nil {
+		return nil, err
+	}
+
+	var movies []map[string]any
+	for _, record := range res.Records {
+		movies = append(movies, record.AsMap())
+	}
+	return movies, nil
 }
 
 func getMoviesBasedOnActor(ctx context.Context, driver neo4j.Driver, dbName, a string) ([]map[string]any, error) {
